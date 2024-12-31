08d17ecb54e98f0861ceda8ceeccbc69f7f376b7
BrunoScaglione <bruno.c.scaglione@gmail.com>
2024-12-31 10:15:15 -0300

    changed licence and sys.path things

--- src/llmops_issue_resolver/cli.py
+++ src/llmops_issue_resolver/cli.py
@@ -1,22 +1,45 @@
 import os
 import sys
+from contextlib import contextmanager
 
 import typer
 
-app = typer.Typer()
 
-# do imports 'assuming' that the package is installed
+@contextmanager
+def temporary_sys_path(path):
+    """
+    A context manager to temporarily add a specified path to the system path.
+    This context manager appends the given path to `sys.path` and ensures that 
+    the original `sys.path` is restored after the context is exited.
+    Args:
+        path (str): The path to be temporarily added to `sys.path`.
+    Yields:
+        None: This context manager does not yield any value.
+    Example:
+        with temporary_sys_path('/some/path'):
+            # Perform operations that require the temporary path
+    """
+    original_sys_path = sys.path.copy()
+    sys.path.append(path)
+    try:
+        yield
+    finally:
+        sys.path = original_sys_path
+
+# do some imports 'assuming' that the package is installed
 # before: 'from agent import ..."
 # now: "from llmops_issue_resolver.agent import ..."
 # But why do this? 
-#     - Because mypy assumes this
-#     - Because it makes it clenar for doing imports within modules that are very deep 
+#     - Because mypy assumes this notation when importing from modules within a package
+#     - Because it makes it cleanar for doing imports within modules that are very deep 
 #     and want to import from modules that are near surface of the package directory 
 #     tree
-# All .py modules need to have this line, but with the more general form would be:
-# sys.path.append(os.join("relative_path_to_llmops_issue_resolver", \
-#     os.path.dirname(__file__)))
-sys.path.append(os.path.dirname(__file__))
+# All .py modules need to have this line, but with the more general form of the import 
+
+# with temporary_sys_path(os.path.dirname(__file__)):
+#    <package_based_imports>
+
+app = typer.Typer()
 
 @app.command()
 def resolve_issue():
